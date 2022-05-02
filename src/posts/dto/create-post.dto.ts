d7ba16fec5bc5aceb6ab8ebfd2c03afb4a30343e
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-05-02 12:32:00 -0300

    Criando a validação dos dados na criação e edição de posts

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,14 @@
+import { IsNotEmpty, IsOptional, IsString, IsEmail } from 'class-validator';
+
 export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsOptional()
   content?: string;
+
+  @IsEmail()
   authorEmail: number;
 }
