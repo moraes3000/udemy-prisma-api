b7bad39e6aedf563a8a902f69fe54e0ea48d77f7
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-05-02 12:13:40 -0300

    Unique Constraint Error

---
+++ src/common/errors/utils/handle-database-erros.util.ts
@@ -0,0 +1,15 @@
+import { UniqueConstraintError } from './../types/UniqueConstraintError';
+import { PrismaClientError } from './../types/PrismaClienteErrors';
+import { DatabaseError } from '../types/DatabaseErros';
+enum PrismaErros {
+  UniqueConstraintFail = 'P2002',
+}
+
+export const handleDatabaseErros = (e: PrismaClientError): Error => {
+  switch (e.code) {
+    case PrismaErros.UniqueConstraintFail:
+      return new UniqueConstraintError(e);
+    default:
+      return new DatabaseError(e.message);
+  }
+};
